6ebb95eddbc3fe9b8328d3a5816ec20491bbff53
Badis91 <146003225+Badis91@users.noreply.github.com>
2025-04-18 09:16:37 +0200

    lol

---
+++ app/src/models/db.js
@@ -0,0 +1,52 @@
+const data = {
+	cars: [
+		{
+			id: 1,
+			numberPlate: 'JKL908',
+			make: 'Porche',
+			model:' 911',
+			year: 2021,
+			color: 'Blue',
+			rentBegin: null,
+			rentEnd: null,
+			rentPrice: 12
+		},
+		{
+			id: 2,
+			numberPlate: 'IOP123',
+			make: 'Peugeot',
+			model: '308',
+			year: 2020,
+			color: 'Red',
+			rentBegin: null,
+			rentEnd: null,
+			rentPrice: 15
+		},
+		{
+			id: 3,
+			numberPlate: 'PHT671',
+			make: 'Ford',
+			model: 'Focus',
+			year: 2019,
+			color: 'Black',
+			rentBegin: null,
+			rentEnd: null,
+			rentPrice: 10
+		},
+		{
+			id: 4,
+			numberPlate: 'WVS321',
+			make: 'Chevrolet',
+			model: 'Malibu',
+			year: 2022,
+			color: 'White',
+			rentBegin: null,
+			rentEnd: null,
+			rentPrice: 18
+		}
+	]
+}
+
+module.exports = {
+	cars: data.cars
+}
